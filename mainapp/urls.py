f9ee0d2d87e87c340d8415e61635baa55be32e6e
skarga0909 <skarga0909@gmail.com>
2022-04-28 13:12:45 +0300

    add lesson 3

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import products
+from .views import products, product
+
+
+app_name = 'mainapp'
 
 urlpatterns = [
-    path('', products, name='products'),
+    path('', products, name='index'),
+    #path('<int:pk>/', product, name='product'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from mainapp.models import Product
+
+
+def product(request, pk):
+    print(pk)
+    return
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': '', 'name': 'все'},
+        {'href': '', 'name': 'дом'},
+        {'href': '', 'name': 'офис'},
+        {'href': '', 'name': 'модерн'},
+        {'href': '', 'name': 'классика'},
+    ]
+    context = {
+        'title': 'каталог',
+        'links_menu': links_menu,
+        'object': Product.objects.get(id=2)
+    }
+    return render(request, 'mainapp/products.html', context=context)
