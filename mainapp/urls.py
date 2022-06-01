ac3ea4b1e4a0ac5044536f32b2ba83a23f2099b7
skarga0909 <skarga0909@gmail.com>
2022-06-01 09:54:09 +0300

    add lesson 2.7

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import products, product
+# from django.views.decorators.cache import cache_page
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', products, name='index'),
     path('category/<int:pk>/', products, name='category'),
+    # path('category/<int:pk>/', cache_page(3600)(products), name='category'),
     path('category/page/<int:page>/', products, name='page'),
     path('<int:pk>/', product, name='detail'),
 ]
